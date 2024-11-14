3b96ae40920a2e6f606e07a39903df8ff067089f
JGar2241271 <joh2241271@maricopa.edu>
2024-11-14 03:01:37 -0700

    rename spin to guess. added vowel and solve loops. initial testing with expected scenarios completed successfully.

--- JGarcia_Guess_The_Word.py
+++ JGarcia_Guess_The_Word.py
@@ -40,7 +40,7 @@ def display_word(word, guessed_letters):
 
 """Function that begins Wheel of Fortune game"""
 def Wheel_Of_Fortune():
-    missed_letters_list = set()
+    used_letters_list = set()
     points = 0
     word, hint = random_word()
 
@@ -49,48 +49,116 @@ def Wheel_Of_Fortune():
     print("Fortune!")
     print(f"\nWelcome to tonight's game, I am your host Pat Sajak and YOU are playing Wheel of Fortune!")
     print("\nThe object of the game is to guess the letters in the mystery word.")
-    print("A random point will be generated (spin) for each turn taken adding or subtracting from your total")
+    print("A random point will be generated for each guess taken adding or subtracting from your total")
     print("A turn is when a consonant is guessed (correctly or incorrectly)")
     print("Vowels can be purchased as long as your score is positive , and cost 50 points a piece ")
+    print("Solving the puzzle correctly will end the game, Solving incorrectly will lose points")
 
 
     print("\n           Lets get started")
     print(f'\nyour word has {len(word)} letters')
-    print(f"Word: {display_word(word.lower(), missed_letters_list)}")
+    print(f"Word: {display_word(word.lower(), used_letters_list)}") #displays mystery word progress
     print(f"\nHint: {hint}")
-    action1 = input("\nPlease select an action: Spin(S) buy a vowel(V)").lower()
+    action1 = input("\nPlease select an action: Guess(G), buy a vowel(V), Solve(S)").lower()
 
+    #While loop, core game function
     while True:
-        print(f"\nCurrent word: {display_word(word.lower(), missed_letters_list)}")
-        print(f"\nCurrent points: {points}")
-        if action1 == "s":
+        print(f"\nCurrent word: {display_word(word.lower(), used_letters_list)}")
+        print(f"Current points: {points}")
+
+        #If loop for guessing words
+        if action1 == "g":
             point = point_system()
-            print(f"for {point} points")
-            action2 = input("please select a consonant").lower()
+            print(f"\nfor {point} points")
+            Consonant = input("\nplease select a consonant").lower()
 
-            if len(action2) != 1:
-                print("Please select a single consonant")
+            #error catcher if user does not enter anything
+            if len(Consonant) != 1:
+                print("\nPlease select a single consonant")
                 continue
-            elif not action2.isalpha():
-                print("Please select a letter")
+            #error catcher if user does not enter a letter
+            if not Consonant.isalpha():
+                print("\nPlease select a letter")
                 continue
-            elif action2 in missed_letters_list:
-                print("\nLetter has already been guessed")
-            elif action2 in word.lower():
-                missed_letters_list.add(action2)
-                points = points + point
-                print(f"\nCorrect! The letter {action2.upper()} is in mystery word")
-            else:
-                missed_letters_list.add(action2)
+
+            #error catcher to see if letter has been used
+            elif Consonant in used_letters_list:
                 points = points - point
-                print(f"\nOops, The letter {action2.upper()} is NOT in mystery word. Minus {point} points")
+                print("\nLetter has already been guessed")
+                print(f"Current word: {display_word(word.lower(), used_letters_list)}")
+                print(f"Hint: {hint}")
+                print(f"Current points: {points}")
+                action1 = input("\nPlease select an action: Guess(G), buy a vowel(V), Solve(S)").lower()
+            #checking if letter in mystery word
+            elif Consonant in word.lower():
+                used_letters_list.add(Consonant)
+                points = points + point
+                print(f"\nCorrect! The letter {Consonant.upper()} is in mystery word. plus {point} points")
+                print(f"Current word: {display_word(word.lower(), used_letters_list)}")
+                print(f"Hint: {hint}")
+                print(f"Current points: {points}")
+                action1 = input("\nPlease select an action: Guess(G), buy a vowel(V), Solve(S)").lower()
+            #catch for letters not in mystery word
+            else:
+                used_letters_list.add(Consonant)
+                points = points - point
+                print(f"\nOops, The letter {Consonant.upper()} is NOT in mystery word. Minus {point} points")
+                print(f"Current word: {display_word(word.lower(), used_letters_list)}")
+                print(f"Hint: {hint}")
+                print(f"Current points: {points}")
+                action1 = input("\nPlease select an action: Guess(G), buy a vowel(V), Solve(S)").lower()
+        #if loop for vowels if v selected in action1
+        elif action1 == "v" and points >= 50:
+            vowel = input("\nPlease select a vowel").lower()
+            if len(vowel) != 1:
+                print("\nPlease select a single vowel")
+                continue
+            if not vowel.isalpha():
+                print("\nPlease select a letter")
+                continue
+            elif vowel in used_letters_list:
+                points = points - point
+                print("\nLetter has already been guessed")
+                print(f"Current word: {display_word(word.lower(), used_letters_list)}")
+                print(f"Hint: {hint}")
+                print(f"Current points: {points}")
+                action1 = input("\nPlease select an action: Guess(G), buy a vowel(V), Solve(S)").lower()
+            elif vowel in word.lower():
+                used_letters_list.add(vowel)
+                points = points - 50
+                print(f"\nCorrect! The letter {vowel.upper()} is in mystery word.")
+                print(f"Current word: {display_word(word.lower(), used_letters_list)}")
+                print(f"Hint: {hint}")
+                print(f"Current points: {points}")
+                action1 = input("\nPlease select an action: Guess(G), buy a vowel(V), Solve(S)").lower()
+            else:
+                used_letters_list.add(vowel)
+                points = points - 50
+                print(f"\nOops, The letter {vowel.upper()} is NOT in mystery word.")
+                action1 = input("\nPlease select an action: Guess(G), buy a vowel(V), Solve(S)").lower()
+        elif action1 == "v" and points <= 50:
+            print("\nYou do not have enough points to buy a vowel")
+            action1 = input("\nPlease select an action: Guess(G), buy a vowel(V), Solve(S)").lower()
+        elif action1 == "s":
+            point = point_system()
+            print(f"\nfor {point} points")
+            solve = input("\nwhat is the mystery word?").lower()
+            if solve != word.lower():
+                points = points - point
+                print(f"\nOops,  {solve.upper()} is NOT in mystery word.")
+                print(f"Current word: {display_word(word.lower(), used_letters_list)}")
+                print(f"Hint: {hint}")
+                print(f"Current points: {points}")
+                action1 = input("\nPlease select an action: Guess(G), buy a vowel(V), Solve(S)").lower()
+            elif solve == word.lower():
+                points = points + point
+                print(f"\nWinner, Winner, Chicken Dinner!.")
+                print(f"Points total: {points}")
+                print("Thank you for playing!")
                 break
-
-
-
-
-
-
+        else:
+            print("\ninvalid entry")
+            action1 = input("\nPlease select an action: Guess(G), buy a vowel(V), Solve(S)").lower()
 
 Wheel_Of_Fortune()
 
